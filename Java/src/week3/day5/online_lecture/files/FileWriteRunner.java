dc8ecaf97312cc58418b8cfc398eaf6bf8c1e446
Hambak-note <dudghks5722@gmail.com>
2022-12-09 13:22:34 +0900

    feat: Add a practice  coe for Udemy lecture java files

---
+++ Java/src/week3/day5/online_lecture/files/FileWriteRunner.java
@@ -0,0 +1,20 @@
+package week3.day5.online_lecture.files;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileWriteRunner {
+
+    public static void main(String[] args) throws IOException {
+        Path pathFileToWrite = Paths.get("src/week3/day5/online_lecture/files/resources/file-write.txt");
+
+        List<String> list = List.of("Apple", "Boy", "Cat", "Dog", "Elephant");
+
+        Files.write(pathFileToWrite, list);
+
+
+    }
+}
