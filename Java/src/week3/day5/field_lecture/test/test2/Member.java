702b6dbd0f08717098084692552e156cd4a78582
Hambak-note <dudghks5722@gmail.com>
2022-12-09 17:34:10 +0900

    feat: Add Member class with ãmember variable and constructor

---
+++ Java/src/week3/day5/field_lecture/test/test2/Member.java
@@ -0,0 +1,18 @@
+package week3.day5.field_lecture.test.test2;
+
+public class Member {
+
+    private String id;
+    private int pw;
+    private String name;
+    private int age;
+
+    public Member(){
+        this.id = null;
+        this.pw = -1;
+        this.name= null;
+        this.age = -1;
+    }
+
+
+}
