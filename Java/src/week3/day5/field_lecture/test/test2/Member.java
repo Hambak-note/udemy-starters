c79df0ae18453b8f406dbd801ab2704db4bf077c
Hambak-note <dudghks5722@gmail.com>
2022-12-09 17:44:49 +0900

    feat: Add ãa login function and a function that outputs a message accordig to the login result

--- Java/src/week3/day5/field_lecture/test/test2/Member.java
+++ Java/src/week3/day5/field_lecture/test/test2/Member.java
@@ -22,11 +22,31 @@ public class Member {
         printInsertInformation();
     }
 
+    public void login(String id, int pw){
+        if(this.id.equals(id)  && this.pw == pw){
+            printLoginOk();
+            return;
+        }
+        printLoginFail();
+    }
+
     private void printInsertInformation(){
         System.out.println("아이디 : " + id + "\n" +
                 "이름 : " + name + "\n" +
                 "나이 : " + age + "\n" +
                 "가입이 완료 되었습니다.");
+    }
+
+    private void printLoginOk(){
+        System.out.println("------------------");
+        System.out.println("로그인 되었습니다.");
+        System.out.println("------------------");
+    }
+
+    private void printLoginFail(){
+        System.out.println("------------------");
+        System.out.println("로그인 정보를 확인하세요.");
+        System.out.println("------------------");
     }
 
 }
