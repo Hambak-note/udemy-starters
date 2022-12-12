d0fc8d8638a8d6724cfed67d386a02b8b7cd0c0d
Hambak-note <dudghks5722@gmail.com>
2022-12-12 16:05:48 +0900

    feat: Add Week4 day1 question1 solving code

---
+++ Java/src/week4/day1/field_lecture/BookMgr.java
@@ -0,0 +1,24 @@
+package week4.day1.field_lecture;
+
+public class BookMgr {
+
+    Book[] booklist;
+
+    public BookMgr(Book[] booklist) {
+        this.booklist = booklist;
+    }
+
+    public void printBooklist(){
+        for(Book book : booklist){
+            System.out.println(book.getTitle());
+        }
+    }
+
+    public void printTotalPrice(){
+        int sum = 0;
+        for(Book book : booklist){
+            sum += book.getPrice();
+        }
+        System.out.println("전체 책 가격의 합 : " + sum);
+    }
+}
